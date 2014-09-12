1853bf90425cf4f23f4c51c33a6cca2f9ff8a0a7
Kristin Onorati <kristin.onorati@gmail.com>
2014-09-12 08:03:24 -0700

    add t.c

--- Lab1/bs.s
+++ Lab1/bs.s
@@ -1,9 +1,12 @@
-    .globl _main                ! IMPORT symbols from C code
-    .globl _getc, _putc         ! EXPORT symbols to C code
+    BOOTSEG = 0x9000            ! Boot block is loaded again to here
+    SSP = 8192                  ! Stack pointer at SS+8KB
+    
+    .globl _main, _prints               ! IMPORT symbols from C code
+    .globl _getc, _putc, _error, _readfd, _getblk, _setes, _inces         ! EXPORT symbols to C code
 
 ! Only one SECTOR loaded at (0000, 7C00). Load entire block to 0x90000
 
-    mov ax,#0x9000              ! set ES to 0x9000
+    mov ax,#BOOTSEG              ! set ES to 0x9000
     mov es,ax
     xor bx,bx                   ! clear BX = 0
 
@@ -15,14 +18,14 @@
     mov ax,#0x0202              ! AH=READ AL=2 sectors
     int 0x13                    ! cal BIOS INT-13
 
-    jmpi start,0x9000           ! CS=0x9000, IP=start
+    jmpi start,BOOTSEG           ! CS=0x9000, IP=start
 
 start:
     mov ax,cs                   ! Set segment registers for CPU
     mov ds,ax                   ! we know ES,CS=0x9000. Let DS=CS
     mov ss,ax                   ! SS = CS ===> all point at 0x9000
     mov es,ax 
-    mov sp,#8192                ! SP = 8192 above SS=0x9000
+    mov sp,#SSP                ! SP = 8192 above SS=0x9000
 
 !------------------ Optional
     mov ax,#0x0012              ! Call BIOS for 640x480 color mode
@@ -40,7 +43,7 @@ _getc:
     int 0x16                    ! call BIOS to get a char in AX
     ret
 
-! void putc(charc) functoin: print a char
+! void putc(charc) function: print a char
 _putc:
     push bp
     mov bp,sp
@@ -53,4 +56,48 @@ _putc:
     pop bp
     ret
 
+! readfd(cyl, head, sector, buf)
+!         4     6     8      10
+_readfd:
+    push bp
+    mov bp,sp                   ! bp = stack frame pointer
     
+    movb dl, #0x00              ! drive 0 = FD0
+    movb dh, 6[bp]              ! head
+    movb cl, 8[bp]              ! sector
+    incb cl
+    movb ch, 4[bp]              ! cyl
+    mov bx, 10[bp]              ! BX=buf ==> memory addr=(ES,BX)
+    mov ax, #0x0202             ! READ 2 sectors to (EX,BX)
+
+    int 0x13                    ! call BIOS to read the block
+    jb _error                   ! to error if CarryBit is on [read failed]
+
+    pop bp
+    ret
+
+! void set_es(unigned short segnment) set ES register to segment
+_setes:
+    push bp
+    mov bp,sp
+    mov ax,4[bp]
+    mov es,ax
+    pop bp
+    ret
+
+! void inces() inc ES by 0x40, or 1kb
+_inces:
+    mov ax,es
+    add ax,#0x40
+    mov es,ax
+    ret
+
+! error & reboot
+_error:
+    mov bx, #bad
+    push bx
+    call _prints
+    int 0x19                    !reboot
+
+bad: .asciz "Error!"
+
--- Lab1/t.c
+++ Lab1/t.c
@@ -1,26 +1,19 @@
 #include "ext2.h"
 
+
 typedef struct ext2_group_desc GD;
 typedef struct ext2_inode INODE;
 typedef struct ext2_dir_entry_2 DIR;
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned long u32;
-
 #define BLK 1024
 
-GD *gp;
-INODE *ip;
-DIR *dp;
-
-int prints(char *s)
+uint8_t prints(char *s)
 {
     while(*s) { putc(*s++);}
     return 1;
 }
 
-int gets(char s[])
+uint8_t gets(char s[])
 {
   while((*s = getc()) != '\r') { putc(*s++); }
   *s = 0;
@@ -28,34 +21,125 @@ int gets(char s[])
   return 1;
 }
 
-u16 get_block(int blk, char buf[])
+uint8_t getblk(uint16_t blk, char buf[])
 {
     //convert blk to (cyl, head, sector)
-    blk *= 2;
-    readfd(blk/36, (blk%36)/18, blk %18, buf);
+    //blk *= 2; 
+    readfd(blk/18, (blk%18)/9, (blk*2)%18, buf);
     return 1;
 }
 
-int main() 
+uint8_t myStrCmp(char *s1, char *s2)
 {
-    u16 i, iblk;
-    char buf1[BLK], buf2[BLK];
+    while(*s1 != 0 && *s2 != 0)
+    {
+        if(*s1++ != *s2++) {return -1;}
+    }
+    return 0;
+}
 
-    prints("booter start\n\r");
+/*uint8_t myStrCpy(char *dest, char *src, uint8_t len)
+{
+    uint8_t i = 0;
+    while(i < len)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = 0;
+    return 1;
+}*/
+
+INODE * search(char *str, INODE *ip, uint16_t iblk)
+{
+    uint8_t i, j;
+    char *cp;
+    DIR *dp;
+    char buf[BLK], temp[64];
+   
+    for(i = 0; i < 12; i++)
+    {
+        if(ip->i_block[i] == 0){break;}
+        getblk((uint16_t)ip->i_block[i], buf);
+        dp = (DIR *)buf;
+        cp = buf;
+
+        while(cp < buf+BLK)
+        {
+            j = 0;
+            while(j < dp->name_len)
+            {
+                temp[j] = dp->name[j];
+                j++;
+            }
+            temp[j]=0;
+            if(myStrCmp(str, temp) == 0)
+            {
+                i = dp->inode;
+                getblk(((i-1)/8) + iblk, buf);
+                return (INODE *)buf + ((i-1)%8);
+            }
+            cp += dp->rec_len;
+            dp = (DIR*)cp;
+        }
+    }
+    return 0;
+}
+
+/*INODE *getInode(uint16_t ino, char buf[], uint16_t iblk)
+{
+    getblk(((ino-1)/8) + iblk, buf);
+    return (INODE *)buf + ((ino-1)%8);
+}*/
+
+uint8_t main() 
+{
+    uint16_t i, iblk;
+    uint32_t *indblk;
+    char buf1[BLK], buf2[BLK];
+    GD *gp;
+    INODE *ip;
 
     /* read blk#2 to get group descriptor 0 */
-    getblk((u16)2, buf1);
+    getblk(2, buf1);
     gp = (GD *)buf1;
-    iblk = (u16)gp->bg_inode_table; // typecase u32 to u16
-    prints("inode_block="); putc(iblk+'0'); getc();
-
-    // 1. Read first indoe block into buf1[]
+    iblk = (uint16_t)gp->bg_inode_table; // typecase u32 to u16
+    
+    // 1. Read first inode block into buf1[]
+    getblk(iblk, buf1);
 
     //2. Let ip point to root INODE (inode #2)
+    ip = (INODE *)buf1 + 1;
 
-    //3. For each DIRECT block of / do:
-        // Read data block into buf2[];
-        // Step through the data block to print the names of dir entries
+    //3. Prompt for filename to boot
+    prints("File: ");
+    gets(buf2);
 
-    //4. prints("\n\rAll done\n\r");
+    //4. Find file in /boot directory
+    ip = search("boot", ip, iblk);
+    if(ip == 0) error();
+    
+    ip = search(buf2, ip, iblk); 
+    if(ip == 0) error();
+    
+    //5. From file's inode find the disk blocks of the file (has direct & indirect blocks)
+    i = ip->i_block[12];
+    getblk(i, buf2);
+    indblk = buf2;
+    
+    setes(0x1000);
+    //6. Load the blocks into memory at the segment 0x1000
+    for(i = 0; i < 12; i++)
+    {
+        getblk(ip->i_block[i], 0);
+        inces();
+    }
+    //Load indirect blocks
+   while(*indblk != 0)
+   {
+       getblk(*indblk, 0);
+       inces();
+       indblk++;
+   }
+     return 1;
 }
